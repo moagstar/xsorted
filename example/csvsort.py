c86247f18e5951460c1eedfc03236da70b4caf49
Daniel Bradburn <moagstar@gmail.com>
2017-01-25 21:43:13 +0100

    simplified csvsort example

--- example/csvsort.py
+++ example/csvsort.py
@@ -16,11 +16,7 @@ if __name__ == '__main__':
     input_path = os.path.join(os.path.dirname(__file__), 'bnc-wordfreq.csv')
     with open(input_path) as fileobj:
         reader = csv.DictReader(fileobj)
-
-        items = list(reader)
-        random.shuffle(items)
-        items = xsorted(items, key=lambda item: int(item['FREQUENCY']))
-
+        items = xsorted(reader, key=lambda item: int(item['FREQUENCY']))
         writer = csv.DictWriter(sys.stdout, reader.fieldnames)
         writer.writeheader()
         for item in items:
