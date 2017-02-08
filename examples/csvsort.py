d8b34eb821b887994b209e3c2cbfcc9f30e42f5a
Daniel Bradburn <moagstar@gmail.com>
2017-02-08 21:15:03 +0100

    fixed error in csvsort example

--- examples/csvsort.py
+++ examples/csvsort.py
@@ -2,10 +2,10 @@
 example showing how xsorted can be used to sort csv files.
 """
 
-import sys, csv, xsorted
+import os, sys, csv, xsorted
 
 
-with open('bnc-wordfreq.csv') as fileobj:
+with open(os.path.join(os.path.dirname(__file__), 'bnc-wordfreq.csv')) as fileobj:
     reader = csv.DictReader(fileobj)
     items = xsorted.xsorted(reader, key=lambda x: int(x['FREQUENCY']))
     writer = csv.DictWriter(sys.stdout, reader.fieldnames)
