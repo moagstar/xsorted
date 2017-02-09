f5f2b6d7b345b5c17bdee68afbd1a0e2698a4cb6
Daniel Bradburn <moagstar@gmail.com>
2017-02-09 21:47:39 +0100

    removed unused files and directories and wrapped examples in if __name__ == '__main__':

--- examples/bigsort.py
+++ examples/bigsort.py
@@ -2,6 +2,8 @@ from random import random
 from six.moves import xrange
 from xsorted import xsorted
 
-nums = (random() for _ in xrange(pow(10, 7)))
-for x in xsorted(nums):
-    pass
+
+if __name__ == '__main__':
+    nums = (random() for _ in xrange(pow(10, 7)))
+    for x in xsorted(nums):
+        pass
--- examples/csvsort.py
+++ examples/csvsort.py
@@ -5,10 +5,11 @@ example showing how xsorted can be used to sort csv files.
 import os, sys, csv, xsorted
 
 
-with open(os.path.join(os.path.dirname(__file__), 'bnc-wordfreq.csv')) as fileobj:
-    reader = csv.DictReader(fileobj)
-    items = xsorted.xsorted(reader, key=lambda x: int(x['FREQUENCY']))
-    writer = csv.DictWriter(sys.stdout, reader.fieldnames)
-    writer.writeheader()
-    for item in items:
-        writer.writerow(item)
+if __name__ == '__main__':
+    with open(os.path.join(os.path.dirname(__file__), 'bnc-wordfreq.csv')) as fileobj:
+        reader = csv.DictReader(fileobj)
+        items = xsorted.xsorted(reader, key=lambda x: int(x['FREQUENCY']))
+        writer = csv.DictWriter(sys.stdout, reader.fieldnames)
+        writer.writeheader()
+        for item in items:
+            writer.writerow(item)
